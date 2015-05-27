25a938508b682e655ec3b655168da1f836373e76
Steve Miller <steventhomasmiller@gmail.com>
2015-05-26 18:34:59 -0700

    With database stuff

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Hello.");
+var sqlite = require ("sqlite3");
+
+var db = new sqlite.Database("test.db" , function() {
+	db.run("CREATE TABLE IF NOT EXISTS users (name, age)");
+
+	db.run("INSERT INTO users VALUES ('Steve', 27)");
+
+	db.get("SELECT * FROM users", function(err, result) {
+		console.log(result);
+	});
+
+	db.all("SELECT * FROM users", function(err, results) {
+		console.log(results);
+	});
+
+
+});
+
+
+
