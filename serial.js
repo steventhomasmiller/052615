f8f3817089fa7a5e7e4a45d2314f406fce74b48e
Steve Miller <steventhomasmiller@gmail.com>
2015-05-26 19:12:05 -0700

    Half way point.

--- serial.js
+++ serial.js
@@ -4,6 +4,26 @@ var sqlite = require("sqlite3");
 var db = new sqlite.Database("serial.db");
 var async = require("async");
 
+async.waterfall([
+	function(c) {
+		db.run("CREATE TABLE IF NOT EXISTS test (x, y)", c);
+	},
+	function(c) {
+		db.run("INSERT INTO test VALUES ($x, $y)", {
+			$x: 3,
+			$y: 4
+		}, c);
+	},
+	function(c) {
+		db.all("SELECT * FROM test", c);
+	},
+	function(result, c){
+		console.log(result);
+	}
+], function(err) {
+	console.log(err);
+})
+
 // db.run("CREATE TABLE IF NOT EXISTS test (x, y)",
 // 	function() {
 // 		db.run("INESRT INTO test VALUES ($x, $y)",{
